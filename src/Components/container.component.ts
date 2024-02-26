6c2140e3878b3f57a61c601868d7131b255eb9da
nasser25 <nasseremil25@gmail.com>
2024-02-26 04:56:51 -0400

    wip container

--- src/Components/container.component.ts
+++ src/Components/container.component.ts
@@ -3,13 +3,48 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'container-component',
     template: `
-    <div class="flex w-screen h-screen bg-black">
-        <h1 class="text-white">Hola mundo desde el contenedor</h1>
-        <router-outlet></router-outlet>
+    <div class="flex items-stretch fixed inset-0 bg-black">
+        <!-- Sidebar -->
+        <div class="flex flex-col w-64 bg-gray-100">
+            <!-- Logo -->
+            <div class="flex justify-center items-center bg-blue-400 w-full p-5">
+                <h1 class="text-2xl font-bold">AcademiK</h1>
+            </div>
+
+            <!-- Menu -->
+            <span *ngFor="let item of menu" class="flex justify-center items-center bg-blue-500 w-full p-5">
+                <i class="fas {{item.icon}}"></i>
+                <a [routerLink]="item.route" class="px-3">{{item.name}}</a>
+            </span>
+        </div>
+        <!-- Content -->
+        <div class="flex flex-col flex-1 items-stretch bg-blue-100">
+            <!-- Header -->
+            <div class="flex items-stretch">
+                <h1 class="text-2xl font-bold">Header</h1>
+            </div>
+            <!-- Main -->
+            <div class="flex-1">
+                <router-outlet></router-outlet>
+            </div>
+        </div>
     </div>
         
     `,
     styles: []
   })
   export class ContainerComponent {
+    menu = [{
+        name: 'Estudiantes',
+        route: 'students',
+        icon: 'fa-solid fa-graduation-cap'
+      },{
+        name: 'Calificaciones',
+        route: 'grades',
+        icon: ''
+      },{
+        name: 'Asistencia',
+        route: 'attendances',
+        icon: ''
+    }];
   }
