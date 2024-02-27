e514efd63e08f7875d7cc7efec6e95690efaf5be
nasser25 <nasseremil25@gmail.com>
2024-02-27 10:26:59 -0400

    models created

---
+++ src/Models/models.ts
@@ -0,0 +1,53 @@
+
+export type Student = {
+    id: number
+    picture: string
+    firstName: string
+    lastName: string,
+    gender: Item,
+    age: number,
+    course: Item
+}
+export type StudentData = {
+    id: number
+    picture: string
+    firstName: string
+    lastName: string,
+    genderId: number,
+    age: number,
+    courseId: number
+}
+export type Attendance = {
+    id: number;
+    date: Date;
+    attended: boolean;
+    student: Item;
+    subject: Item;
+};
+export type AttendanceData = {
+    id: number;
+    date: Date;
+    attended: boolean;
+    studentId: number;
+    subjectId: number;
+};
+
+export type Grade = {
+    id: number;
+    score: number;
+    letterGrade: string;
+    student: Item;
+    subject: Item;
+};
+export type GradeData = {
+    id: number;
+    score: number;
+    letterGrade: string;
+    studentId: number;
+    subjectId: number;
+};
+
+export type Item = {
+    id: number
+    name: string
+}
