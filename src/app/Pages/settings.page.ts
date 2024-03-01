9274ec6cfd2c2c44667bc54668f790843d907513
nasser25 <nasseremil25@gmail.com>
2024-03-01 05:56:15 -0400

    La payola

--- src/app/Pages/settings.page.ts
+++ src/app/Pages/settings.page.ts
@@ -3,7 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'settings-page.ts',
     template: `
-       <h1 class="text-purple-500" >Hola mundo desde settings page</h1> 
+       <div class="flex flex-col h-[80vh] justify-center items-center space-y-4">
+         <img src="../../assets/nasserfoto.jpg" alt="Nasser foto" class="h-24 w-24 rounded-full shadow object-cover">
+         <h1>Nasser Emil Issa Tavares</h1>
+         <p>Software Engineer</p>
+         <p>Full Stack Developer</p>
+         <p>nasseremil25@gmail.com</p>
+         <p>829-805-8311</p>
+       </div>
     `,
     styles: []
   })
